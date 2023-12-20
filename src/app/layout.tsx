29b1a4d8f64a12a54adb510d4b5bd5fd68504332
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-12-20 17:16:34 +0100

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata: Metadata = {
   applicationName: "Next.js 14 SEO Checklist",
   referrer: "origin-when-cross-origin",
   keywords: ["Next.js", "SEO", "React", "Performance"],
-  authors: [{ name: "Alex Hramovich" }],
+  images: ["https://cdn.sanity.io/images/0xiy76wv/production/74e31f73a4cd86c932951002ef121b666da5c526-2298x1137.png?auto=format"],
+authors: [{ name: "Alex Hramovich" }],
   creator: "Alex Hramovich",
   publisher: "FocusReactive",
   formatDetection: {
