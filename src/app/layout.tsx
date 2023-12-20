f2d64436e1dc9d38e0850b1eed27dd8a39699936
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-12-20 17:20:34 +0100

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: {
     template: "%s | Next.js 14 SEO checklist",
-    default: "Next.js 14 SEO checklist article",
+    default: "Next.js 14 SEO checklist",
   },
   generator: "Next.js",
   applicationName: "Next.js 14 SEO Checklist",
   referrer: "origin-when-cross-origin",
   keywords: ["Next.js", "SEO", "React", "Performance"],
-  images: ["https://cdn.sanity.io/images/0xiy76wv/production/74e31f73a4cd86c932951002ef121b666da5c526-2298x1137.png?auto=format"],
-authors: [{ name: "Alex Hramovich" }],
+  authors: [{ name: "Alex Hramovich" }],
   creator: "Alex Hramovich",
   publisher: "FocusReactive",
   formatDetection: {
@@ -31,6 +30,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body className={`${inter.className} bg-white dark:bg-black`}>
         {children}
       </body>
