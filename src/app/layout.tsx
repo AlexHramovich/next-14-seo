5e42b5c8dd081386732addc217b0664f2024fbb8
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-12-20 17:25:40 +0100

    update home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
     template: "%s | Next.js 14 SEO checklist",
     default: "Next.js 14 SEO checklist",
   },
+  description:
+    "All things you need to know about SEO in Next.js 14 to make your website rank higher in search engines.",
   generator: "Next.js",
   applicationName: "Next.js 14 SEO Checklist",
   referrer: "origin-when-cross-origin",
@@ -20,6 +22,18 @@ export const metadata: Metadata = {
     email: false,
     address: false,
     telephone: false,
+  },
+
+  openGraph: {
+    title: "Next 14 SEO Checklist",
+    description:
+      "All things you need to know about SEO in Next.js 14 to make your website rank higher in search engines.",
+    url: "https://next-14-seo.vercel.app",
+    siteName: "Next.js 14 SEO Checklist",
+    type: "website",
+    images: [
+      "https://cdn.sanity.io/images/0xiy76wv/production/74e31f73a4cd86c932951002ef121b666da5c526-2298x1137.png?auto=format",
+    ],
   },
 };
 
