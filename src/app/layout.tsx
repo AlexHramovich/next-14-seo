1e9e7f9181ee4ee457ebd493e444e34b8a52e4f0
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-12-26 10:04:56 +0100

    update captions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Next.js 14 SEO checklist",
-    default: "Next.js 14 SEO checklist",
+    template: "%s | Next.js SEO checklist",
+    default: "Next.js SEO checklist",
   },
   description:
     "All things you need to know about SEO in Next.js 14 to make your website rank higher in search engines.",
   generator: "Next.js",
-  applicationName: "Next.js 14 SEO Checklist",
+  applicationName: "Next.js SEO Checklist",
   referrer: "origin-when-cross-origin",
   keywords: ["Next.js", "SEO", "React", "Performance"],
   authors: [{ name: "Alex Hramovich" }],
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     description:
       "All things you need to know about SEO in Next.js 14 to make your website rank higher in search engines.",
     url: "https://next-14-seo.vercel.app",
-    siteName: "Next.js 14 SEO Checklist",
+    siteName: "Next.js SEO Checklist",
     type: "website",
     images: [
       "https://cdn.sanity.io/images/0xiy76wv/production/74e31f73a4cd86c932951002ef121b666da5c526-2298x1137.png?auto=format",
--- src/components/TopicsGrid/index.tsx
+++ src/components/TopicsGrid/index.tsx
@@ -1,50 +1,53 @@
+"use client";
+
 import Link from "next/link";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 
 const TOPICS = [
   {
-    title: "Rendering strategy",
+    title: "1. Rendering strategy",
     description:
       "Strategies and techniques for efficient rendering of web pages.",
     link: "/rendering",
   },
   {
-    title: "Meta tags",
+    title: "2. Meta tags",
     description:
       "Including title, description, canonical tags for SEO and web page identification.",
     link: "/meta-tags",
   },
   {
-    title: "Sitemap & robots.txt",
+    title: "3. Sitemap & robots.txt",
     description:
       "Tools for guiding search engines in website navigation and indexing.",
     link: "/sitemap-robots",
   },
   {
-    title: "Canonical & Localization tags",
+    title: "4. Canonical & Localization tags",
     description:
       "localization practices, possibly using 'ahref' for multilingual support.",
     link: "/canonical-localization",
   },
   {
-    title: "Images, fonts and scripts optimizations",
+    title: "5. Images, fonts and scripts optimizations",
     description:
       "Optimizing images, fonts and scripts for better performance and load times.",
     link: "/fonts-images",
   },
   {
-    title: "Core web vitals",
+    title: "6. Core web vitals",
     description:
       "Essential metrics for a healthy, user-friendly web experience.",
     link: "/core-web-vitals",
   },
   {
-    title: "Structured data",
+    title: "7. Structured data",
     description:
       "Implementing structured data for enhanced search engine understanding.",
     link: "/structured-data",
   },
   {
-    title: "Redirects",
+    title: "8. Redirects",
     description:
       "Effective use of redirects for site structure and URL management.",
     link: "/redirects",
@@ -58,16 +61,48 @@ const TOPICS = [
 ];
 
 const TopicsGrid: React.FC = () => {
+  const [activeTopics, setActiveTopics] = useState<string[]>([]);
+
+  useEffect(() => {
+    const localStoraged = localStorage.getItem("completedTopics");
+    if (localStoraged) {
+      setActiveTopics(JSON.parse(localStoraged));
+    }
+  }, []);
+
+  const onTopicClick = (e: MouseEvent<HTMLDivElement>, topic: string) => {
+    e.stopPropagation();
+
+    let newActiveTopics = [...activeTopics];
+
+    const index = newActiveTopics.indexOf(topic);
+
+    if (index > -1) {
+      newActiveTopics = newActiveTopics.filter((t) => t !== topic);
+    } else {
+      newActiveTopics.push(topic);
+    }
+
+    localStorage.setItem("completedTopics", JSON.stringify(newActiveTopics));
+
+    setActiveTopics(newActiveTopics);
+  };
+
   return (
     <div className="grid grid-flow-row-dense gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-clos-1">
       {TOPICS.map((topic) => {
         if (topic.type === "external")
           return (
-            <Link key={topic.title} href={topic.link} target="_blank" className="group text-clip hover">
+            <Link
+              key={topic.title}
+              href={topic.link}
+              target="_blank"
+              className="group text-clip hover"
+            >
               <div className="card h-full w-full bg-slate-800 text-primary-content">
                 <div className="card-body">
                   <h2 className="card-title text-white">{topic.title}</h2>
-                  <p className="text-emerald-600 font-bold group-hover:underline">
+                  <p className="text-emerald-600 font-bold group-hover:underline z-10">
                     {topic.description}
                   </p>
                 </div>
@@ -79,11 +114,21 @@ const TopicsGrid: React.FC = () => {
           <Link key={topic.title} href={topic.link}>
             <div
               key={topic.title}
-              className="card glass h-full w-full bg-base-100 shadow-xl hover:bg-primary hover:text-primary-content transition-all"
+              className="card relative glass h-full w-full bg-base-100 shadow-xl hover:bg-primary hover:text-primary-content transition-all"
             >
               <div className="card-body">
                 <h2 className="card-title">{topic.title}</h2>
                 <p>{topic.description}</p>
+              </div>
+              <div
+                className="absolute top-2 right-2 bg-base-100 h-[28px] p-[2px] rounded-[10px]"
+                onClick={(e) => onTopicClick(e, topic.title)}
+              >
+                <input
+                  type="checkbox"
+                  checked={activeTopics.includes(topic.title)}
+                  className="checkbox p-0 m-0 checkbox-success"
+                />
               </div>
             </div>
           </Link>
